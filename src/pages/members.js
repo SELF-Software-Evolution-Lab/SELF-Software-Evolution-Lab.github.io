f5e852d424b15be509ed83a11f138bf6903b5b1b
Kelly Garces <kellygarce@gmail.com>
2020-01-22 16:50:48 -0500

    Modificado estatus de Jose y Giacomo

--- src/pages/members.js
+++ src/pages/members.js
@@ -13,14 +13,36 @@ export default ({ data }) => {
     <Fragment>
       <Header/>
       <Container className="mt-3">
-        <h1>Professors</h1>
+        <h1>Professors from the Systems and Computing Engineering Department (Uniandes)</h1>
         <CardDeck>
           {data.allMembersJson.edges.map(n => {
-            if (n.node.category === "professor")
+            if (n.node.category === "discProfessor")
               return <Member key={n.node.name} node={n.node}/>;
             return "";
           })}
         </CardDeck>
+        {data.allMembersJson.edges.find(n => n.node.category === "professor") ?
+          <Fragment>
+            <h1>Other professors</h1>
+            <ListGroup variant="flush">
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "professor")
+                  return <MemberNoImage key={n.node.name} node={n.node}/>;
+                return "";
+              })}
+            </ListGroup>
+          </Fragment> : ""}
+        {data.allMembersJson.edges.find(n => n.node.category === "postdoc") ?
+          <Fragment>
+            <h1>Post doc</h1>
+            <ListGroup variant="flush">
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "postdoc")
+                  return <MemberNoImage key={n.node.name} node={n.node}/>;
+                return "";
+              })}
+            </ListGroup>
+          </Fragment> : ""}
         {data.allMembersJson.edges.find(n => n.node.category === "phd") ?
           <Fragment>
             <h1>PHD Candidates</h1>
